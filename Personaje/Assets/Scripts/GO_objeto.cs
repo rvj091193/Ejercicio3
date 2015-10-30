542f78db6dc2d4310fac3432bab6e13e8752de21
Raul <raulvidana@hotmail.com>
2015-10-30 13:51:40 -0600

    Ejercicios de Unity

---
+++ Personaje/Assets/Scripts/GO_objeto.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GO_objeto : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 20);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Personaje/Assets/Scripts/move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public float speed;
+	public float dir;
+
+	// Use this for initialization
+	void Start () {
+				speed = 3;
+				dir = 1;
+		}
+	
+	// Update is called once per frame
+	void Update () { 
+			moverSiempre ();	
+	}
+	//funcion que mueve al personaje
+	public void moveLe()
+	{
+				//Debug.Log ("Hola ");
+				//this.transform.Translate((Time.deltaTime * speed) * Vector3.left);
+			dir = -1;
+		}
+	public void moveR()
+	{
+			dir = 1; 
+				//this.transform.Translate ((Time.deltaTime * speed) * Vector3.right);
+		}
+	public void moverSiempre()
+		{
+		this.transform.Translate ((Time.deltaTime * speed * dir) * Vector3.right);
+		}
+}
